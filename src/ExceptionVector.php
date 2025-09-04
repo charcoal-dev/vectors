5d6a36ae6ce547abe43e68c4d0b9768341d4da95
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-04 20:24:37 +0500

    Add `ExceptionVector` class to manage collections of exceptions

---
+++ src/ExceptionVector.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Part of the "charcoal-dev/base" package.
+ * @link https://github.com/charcoal-dev/base
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Vectors;
+
+use Charcoal\Contracts\Vectors\VectorInterface;
+
+/**
+ * A vector-like structure that stores exceptions.
+ * @implements VectorInterface<\Throwable>
+ */
+class ExceptionVector implements VectorInterface
+{
+    private array $vector;
+
+    /**
+     * @param \Throwable ...$exceptions
+     */
+    public function __construct(\Throwable ...$exceptions)
+    {
+        $this->vector = $exceptions;
+    }
+
+    /**
+     * @param \Throwable $exception
+     * @return $this
+     */
+    public function append(\Throwable $exception): static
+    {
+        $this->vector[] = $exception;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->vector);
+    }
+
+    /**
+     * @return \Traversable<int,\Throwable>
+     */
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->vector);
+    }
+
+    /**
+     * @return array<int,\Throwable>
+     */
+    public function getArray(): array
+    {
+        return $this->vector;
+    }
+}
