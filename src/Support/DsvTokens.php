c523b57cb4a71a1a8919b0760573de935ab9f56c
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-04 21:30:44 +0500

    Add `DsvTokens` class for managing delimiter-separated value tokens

---
+++ src/Support/DsvTokens.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Part of the "charcoal-dev/vectors" package.
+ * @link https://github.com/charcoal-dev/vectors
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Vectors\Support;
+
+use Charcoal\Base\Vectors\AbstractTokensVector;
+
+/**
+ * Represents a specialized collection for handling delimiter-separated value (DSV) tokens.
+ * Extends the functionality of AbstractTokensVector to manage token addition and deletion operations.
+ */
+final class DsvTokens extends AbstractTokensVector
+{
+    /**
+     * @param string ...$tokens
+     * @return self
+     */
+    public function add(string ...$tokens): self
+    {
+        return $this->addTokens(...$tokens);
+    }
+
+    /**
+     * @param string $token
+     * @return bool
+     */
+    public function delete(string $token): bool
+    {
+        return $this->deleteToken($token);
+    }
+}
